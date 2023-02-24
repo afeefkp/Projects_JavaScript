7b58f4afd158117455a9ed0db3b4d7e8416ea027
afeefkp <93336792+afeefkp@users.noreply.github.com>
2023-02-24 11:49:06 +0530

    Add files via upload
    Platform counter project

---
+++ Project 1/index.js
@@ -0,0 +1,10 @@
+
+
+
+function increment(){
+    let count=parseInt(document.getElementById("count").innerText) ;
+    document.getElementById("count").innerText=count+1;
+}
+function reset(){
+    document.getElementById("count").innerText=0;
+}
